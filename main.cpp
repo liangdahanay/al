d99b595769578f9ac29779cdba218823afb8e987
liangdahanay <liangdaleo@gmail.com>
2015-02-08 18:52:11 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 8, 2015
+ *      Author: liang
+ */
+#include "lowestCommonAncestor.h"
+int main(int argc, char** argv){
+	lowestCommonAncestorTest();
+	return 0;
+}
+
+
+
