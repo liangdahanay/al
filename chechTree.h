7f442be493ee6e3e83752240d0cf95a205e4d9ba
liangdahanay <liangdaleo@gmail.com>
2015-02-09 20:23:34 -0600

    Create chechTree.h

---
+++ chechTree.h
@@ -0,0 +1,13 @@
+#ifndef CHECKTREE_H_
+#define CHECKTREE_H_
+
+#include "tree.h"
+
+bool checkBalancedDiff(Node* root);
+bool checkBalanced(Node* root);
+bool checkBinaryInPlace(Node* root);
+bool checkBinaryExtraSpace(Node* root);
+
+void checkTreeTest();
+
+#endif
