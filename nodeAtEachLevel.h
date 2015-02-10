688970cbc257216a63d99f829c3696e22cbb7f1c
liangdahanay <liangdaleo@gmail.com>
2015-02-09 21:47:19 -0600

    Create nodeAtEachLevel.h

---
+++ nodeAtEachLevel.h
@@ -0,0 +1,17 @@
+/*
+ * nodeAtEachLevel.h
+ *
+ *  Created on: Feb 9, 2015
+ *      Author: liang
+ */
+
+#ifndef NODEATEACHLEVEL_H_
+#define NODEATEACHLEVEL_H_
+#include "tree.h"
+#include "vector"
+using namespace std;
+vector<vector<Node*> > nodeEachLevel(Node*);
+void nodeEachLevelTest();
+
+
+#endif /* NODEATEACHLEVEL_H_ */
