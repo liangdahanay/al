377f402d25973b057014da21cd0229290ad321ad
liangdahanay <liangdaleo@gmail.com>
2015-02-09 20:22:36 -0600

    Update lowestCommonAncestor.h

--- lowestCommonAncestor.h
+++ lowestCommonAncestor.h
@@ -1,16 +1,14 @@
+#ifndef LOWESTCOMMOMANCESTOR_H_
+#define LOWESTCOMMOMANCESTOR_H_
+#include "tree.h"
 #include <vector>
 
 using namespace std;
 
-struct Node{
-	int val;
-	Node* left;
-	Node* right;
-	Node(int, Node*, Node*);
-};
-Node* constructTree(const vector<int>& num);
-Node* getByValue(Node* root, int val);
+
 Node* LCAUpDown(Node* root, Node* p, Node* q);
 Node* LCADownUp(Node* root, Node* p, Node* q);
 Node* LCABST(Node* root, Node* p, Node* q);
 void lowestCommonAncestorTest();
+
+#endif
